a945c0f49487a17b853f23dee6b2edbb42f7771c
Enrique Barba <35747594+enriquebarba97@users.noreply.github.com>
2021-05-20 09:48:05 +0200

    Added env options for local docker-compose deploy

--- files-frontend/src/services/auth.service.js
+++ files-frontend/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://apifiles.enriquexperiments.com/api/user/";
+const API_URL = process.env.REACT_APP_API_URL + "user/";
 
 class AuthService {
   login(username, password) {
